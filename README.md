7b609386ad2b95a8132ac4d57b5f95684c65d3cc
lukas1326 <aprilium@live.com>
2020-09-30 10:01:10 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## My pagedown rendered CV
+
+This repo contains the source-code and results of my CV built with the [pagedown package](https://pagedown.rbind.io).
+
+
+CV link: https://lukas1326.github.io/cv/
+
---
+++ common_variables.R
@@ -0,0 +1,15 @@
+# Common non-csv data between CV and resume
+
+# Header with description of what is trying to be accomplished with this CV/Resume
+intro_text <- "I enjoy working with data, mastering programming on my own, learning machine and deep learning algorithms to bring my favorite part of my work related to analytics, marketing research and business process research to the most effective level. Experience in various areas of business has given me the opportunity to understand its needs, learn to communicate effectively, be a reliable member of the team.
+
+"
+
+
+# Language skills for skill bars visualization
+skills <- tribble(
+  ~skill,               ~level,
+  "English",                  3,
+  "Ukrainian",                5,
+  "Russian",                  5,
+ )
